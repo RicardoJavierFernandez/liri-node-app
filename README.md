2e33a7cae180d291a1adb332a20b70c84a85bd6e
RicardoJavierFernandez <rickyjf808@gmail.com>
2019-08-18 16:28:39 -0400

    Added data validation with error feedback to the user

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 # liri-node-app
 
-This is a Node Js CLI application that allows the user to enter four commands.
-    * concert-this [artist/band name here]
-    * spotify-this-song [song name here]
-    * movie-this [movie name here]
-    * do-what-it-says [do-what-it-say]
+This is a Node.js command line application that allows the user to enter four different commands:
+* *concert-this [artist/band name here]*
+    ![concert-this](./Examples/concert-this_WithArtistName.png)
+* *spotify-this-song [song name here]*
+    ![spotify-this-song](./Examples/spotify-this-song_WithSongName.png)
+* *movie-this [movie name here]*
+    ![movie-this](./Examples/movie-this_WithMovieName.png)
+* *do-what-it-says*
+    ![spotify-this](./Examples/do-what-it-says.png)
+
+APIs used for this project were:
+* Spotify
+* Bands In Town
+* OMDb
+
+Node Packages used:
+* axios
+* fs
+* node-spotify-api
+* moment
+
--- liri.js
+++ liri.js
@@ -1,14 +1,20 @@
 // https://developer.spotify.com/documentation/web-api/quick-start/
 
 require("dotenv").config();
+
 const Spotify = require('node-spotify-api');
 const axios = require('axios');
 const moment = require('moment');
+const fs = require('fs');
 var keys = require("./keys.js");
+
 var spotify = new Spotify(keys.spotify);
 var ombd_key = keys.omdb.key;
 
+// Command line argument
+var userRequest = process.argv[2];
 
+// Call the Bands In Town API and return concert information
 function bandsInTown(artistBandName)
 {
     let bandsUrl = `https://rest.bandsintown.com/artists/${artistBandName}/events?app_id=codingbootcamp`
@@ -20,7 +26,8 @@ function bandsInTown(artistBandName)
             {
                 console.log('Venue: ' + concert.venue.name
                         + '\nLocation: ' + concert.venue.city + ', ' + concert.venue.country
-                        + '\nDate: ' + moment(concert.datetime).format('MM/DD/YYYY'));
+                        + '\nDate: ' + moment(concert.datetime).format('MM/DD/YYYY')
+                        + '\n');
             }
         })
         .catch(error => {
@@ -28,7 +35,7 @@ function bandsInTown(artistBandName)
         })
 };
 
-
+// Call the OMDb API and return movie information
 function omdbRequest(movieTitle)
 {
     let omdbUrl = `http://www.omdbapi.com/?t=${movieTitle}&apikey=${ombd_key}`
@@ -52,14 +59,15 @@ function omdbRequest(movieTitle)
                         + '\nCountry: ' + movieData.Country
                         + '\nLanguage: ' + movieData.Language
                         + '\nPlot: ' + movieData.Plot
-                        + '\nActors: ' + movieData.Actors);
+                        + '\nActors: ' + movieData.Actors
+                        + '\n');
         })
         .catch(error => {
             console.log('An error occured', error);
         })
 };
 
-
+// Call the Spotify API and return song information
 function spotifyRequest(songName)
 {
     spotify
@@ -69,74 +77,95 @@ function spotifyRequest(songName)
             limit: 5
         })
         .then(response => {
-            // console.log(response.tracks.items);
             let songMatches = response.tracks.items;
 
             for (match of songMatches)
-            {
-                
+            {   
                 console.log('Artist: ' + match.artists[0].name
                         + '\nSong Name: ' + match.name
                         + '\nLink: ' + match.href
                         + '\nAlbum: ' + match.album.name
-                        + '\n')
+                        + '\n');
             }
-
-            // for (let i = 0; i < songMatches.length; i++)
-            // {
-            //     let songData = songMatches[i]
-            //     console.log('Artist: ' + songData.artists[0]
-            //                 + '\nSong Name: ' + songData.name
-            //                 + '\nLink: ' + songData.href
-            //                 + '\nAlbum: ' + songData.album)
-            // }
         })
         .catch(error => {
             console.log('An error occured:', error);
         })
-}
+};
 
-function liriRequest()
+// Reads a text file with a default request to the Spotify API and returns song information
+function doWhatItIs()
 {
+    fs.readFile('./random.txt', 'utf-8', (error, data) => {
+        if (error) 
+        {
+            console.log('An error occured:', error);
+        }
+        else
+        {
+            let request = data.split(',');
+            liriRequest(request[0], request[1]);
+        }
+    });
+};
+
+// Main function that makes the application work
+function liriRequest(request)
+{    
     if (process.argv.length >= 4)
     {
-        // Command line arguments
-        let userRequest = process.argv[2];
-        let userParameters = process.argv.slice(3).join(' '); // create a string of the user arguments to pass to the APIs
+        // Create a string of the user arguments to pass to the APIs
+        let userParameters = process.argv.slice(3).join(' '); 
 
-        if (userRequest === 'concert-this')
+        if (request === 'concert-this')
         {
             bandsInTown(userParameters);
         }
-        else if (userRequest == 'spotify-this-song')
+        else if (request == 'spotify-this-song')
         {
             spotifyRequest(userParameters);
         }
-        else if (userRequest == 'movie-this')
+        else if (request == 'movie-this')
         {
-            omdbRequest(userParameters); //.replace(' ', '+')
-        }
-        else if (userRequest == 'do-what-it-says')
-        {
-            console.log('do-what-it-says');
+            omdbRequest(userParameters);
         }
         else 
-        {
-            console.log('Not a valid request please type on of the following followed by a search term'
-                        + 'concert-this'
-                        + 'spotify-this-song'
-                        + 'movie-this'
-                        + 'do-what-it-says');
+        {            
+            console.log('Not a valid request please type on of the following commands followed by a search term:'
+                        + '\n concert-this <artist/band name here>'
+                        + '\n spotify-this-song <song name here>'
+                        + '\n movie-this <movie name here>'
+                        + '\n do-what-it-says');
         }
     }  
     else 
     {
-        console.log('Please type one of the following followed by a search term'
-                    + 'concert-this <artist/band name here>'
-                    + 'spotify-this-song <song name here>'
-                    + 'movie-this <movie name here>'
-                    + 'do-what-it-says');
+        if (request === 'concert-this')
+        {
+            console.log('Looks like you are missing the name of the artist/band. Please try again with an artist/band name.');
+        }
+        else if (request == 'spotify-this-song')
+        {
+            spotifyRequest('The Sign by Ace of Base');
+        }
+        else if (request == 'movie-this')
+        {
+            console.log('Looks like you are missing the name of a movie. Please try again with a movie name.');
+        }
+        else if (request == 'do-what-it-says')
+        {
+            doWhatItIs();
+        }
+        else 
+        {
+            console.log('Please type one of the following commands followed by a search terms:'
+            + '\n concert-this <artist/band name here>'
+            + '\n spotify-this-song <song name here>'
+            + '\n movie-this <movie name here>'
+            + '\n do-what-it-says');
+        }
     } 
-}
+};
 
-liriRequest();
+
+liriRequest(userRequest);
